22beac04bc7d6e3ef92c850670b45a73236e20f3
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-03-20 15:22:29 +0000

    add race logic with age and registration conditions

---
+++ race.js
@@ -0,0 +1,19 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+
+registeredEarly = false;
+
+runnerAge = 18;
+
+if (registeredEarly && runnerAge > 18) {
+  raceNumber += 1000;
+}
+
+if (registeredEarly && runnerAge > 18) {
+  console.log(`You will race at 9:30am. Your race number is ${raceNumber}`)
+} else if (runnerAge > 18 && !registeredEarly) {
+  console.log(`You will race at 11:00am. Your race number is ${raceNumber}`)
+} else if (runnerAge < 18) {
+  console.log(`You will race at 12:30pm. Your race number is ${raceNumber}`)
+} else if (runnerAge === 18) {
+  console.log('Please see the registration desk')
+}
